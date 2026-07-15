06d900be7239428df13d7b92d12f421b15fc7606
SudiptoRahaOfficial <sudiptoraha.official@gmail.com>
2026-07-16 02:32:56 +0600

    maked navbar responsive for all device

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,11 +4,15 @@ import Logo from './Logo'
 function Navbar() {
 	return (
 		<nav className='bg-indigo-300 py-2'>
-			<div className='container w-3/4 m-auto flex justify-between items-center'>
+			<div className='w-14/15 sm:w-3/4 m-auto flex justify-between items-center'>
 				<Logo />
-				<ul className='flex gap-5'>
-					<li className='font-medium cursor-pointer'>Home</li>
-					<li className='font-medium cursor-pointer'>Your Tasks</li>
+				<ul className='flex gap-4 sm:gap-6'>
+					<li className='text-sm sm:text-base font-medium cursor-pointer'>
+						Home
+					</li>
+					<li className='text-sm sm:text-base font-medium cursor-pointer'>
+						Your Tasks
+					</li>
 				</ul>
 			</div>
 		</nav>
