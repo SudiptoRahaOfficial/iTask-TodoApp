7c36a3250ccfa5cb4ac6d59eff309efebb1241cd
SudiptoRahaOfficial <sudiptoraha.official@gmail.com>
2026-07-15 12:39:58 +0600

    styling fixing on tasklinting section

--- client/src/components/TaskListing.jsx
+++ client/src/components/TaskListing.jsx
@@ -7,7 +7,7 @@ function TaskListing({ tasks }) {
 		<div className='w-3/5 m-auto bg-yellow-200 p-8 rounded-lg my-5'>
 			<SectionHeading value={'List Of Your Tasks'} />
 			{tasks.length <= 0 ? (
-				<p className='font-medium text-md bg-white text-center w-1/4 mx-auto mt-4 py-2 rounded-sm'>
+				<p className='font-medium text-md text-gray-500! bg-white text-center w-1/4 mx-auto mt-4 py-2 rounded-sm'>
 					No Tasks Found!
 				</p>
 			) : (
